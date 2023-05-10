932a4cd56f66bc82a46cbe15054eeb76a0dbd7f2
Lucas Eiji <104649020+eijilucas@users.noreply.github.com>
2023-05-10 10:48:54 -0300

    Update Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -10,5 +10,38 @@ package control;
  * @author lucas
  */
 public class Calculadora {
+    private double v1;
+    private double v2;
     
+    public double getV1() {
+        return v1;
+    }
+
+    public void setV1(double v1) {
+        this.v1 = v1;
+    }
+
+    public double getV2() {
+        return v2;
+    }
+
+    public void setV2(double v2) {
+        this.v2 = v2;
+    }
+    
+    public double somar() {
+        return v1 + v2;
+    }
+    
+    public double subtrair() {
+        return v1 - v2;
+    }
+    
+    public double multiplicar() {
+        return v1 * v2;
+    }
+    
+    public double dividir() {
+        return v1 / v2;
+    }
 }
